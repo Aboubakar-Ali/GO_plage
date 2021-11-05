67f5752eb05ba7e301ca4f2c060d47cd4025c21e
Aboubakar Ali <abdoulazizdjankado.aboubakarali@ynov.com>
2021-11-05 15:30:05 +0100

    i love my mom

---
+++ Pgn.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var n, max int
+	max = 0
+	for i := 1; i < 11; i++ {
+		fmt.Println("entrez un nombre")
+		fmt.Scan(&n)
+		if i == 1 || n > max {
+			max = n
+		}
+	}
+	fmt.Println("le nombre le plus grand est", max)
+}
